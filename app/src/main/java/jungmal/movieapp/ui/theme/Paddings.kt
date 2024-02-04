5d8fd9e96d174ac9f62ab207fc8433d585952984
jungmal <jungmal@yonsei.ac.kr>
2024-02-04 20:53:16 +0900

    custom padding 설정

---
+++ app/src/main/java/jungmal/movieapp/ui/theme/Paddings.kt
@@ -0,0 +1,13 @@
+package jungmal.movieapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Paddings {
+    val none = 0.dp
+    val xsmall = 2.dp
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 12.dp
+    val extra = 24.dp
+    val xextra = 32.dp
+}
